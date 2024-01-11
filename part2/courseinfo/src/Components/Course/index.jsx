c9a89a0846e30a7e031da97c4a97786bf832938c
lsot ukow <939991685@qq.com>
2024-01-12 00:29:22 +0200

    2.2-2.5

--- part2/courseinfo/src/Components/Course/index.jsx
+++ part2/courseinfo/src/Components/Course/index.jsx
@@ -1,20 +1,40 @@
-const Course = ({ course }) => {
-    return <div>
-        <Header text={course.name} />
-        <Content parts={course.parts} />
-    </div>
+import { useState } from 'react'
+
+
+const Course = ({ courses }) => {
+
+    return <>
+        <h1>Web Development Curriculum</h1>
+        {courses.map(course => (
+            <>
+                <Header text={course.name} />
+                <Content parts={course.parts} />
+
+            </>
+        ))}
+    </>
 }
 
 const Header = ({ text }) => {
-    return <h1>{text}</h1>
+    return <h2>{text}</h2>
 }
 
 const Content = ({ parts }) => {
-    return parts.map(part => <Part part={part} />)
+    const total = parts.reduce((sum, part) => sum + part.exercises, 0)
+    return <div>
+        {parts.map((part, i) => {
+            return <Part key={i} part={part} />
+        })}
+
+        <div><b>Total of {total} exercises</b></div >
+    </div>
 }
+
+
 
 const Part = ({ part }) => {
     return <div>{part.name} {part.exercises}</div>
 }
+
 
 export default Course
