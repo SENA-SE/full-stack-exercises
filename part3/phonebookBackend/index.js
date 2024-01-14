a400d8a91150fac6ce458b2fc5b9a72798732b8b
lsot ukow <939991685@qq.com>
2024-01-14 22:23:38 +0200

    3.7-3.8

--- part3/phonebookBackend/index.js
+++ part3/phonebookBackend/index.js
@@ -1,37 +1,52 @@
 // const http = require('http')
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
+
+// const requestLogger = (request, response, next) => {
+//     console.log('Method:', request.method)
+//     console.log('Path:  ', request.path)
+//     console.log('Body:  ', request.body)
+//     console.log('---')
+//     next()
+// }
+// const unknownEndpoint = (request, response) => {
+//     response.status(404).send({ error: 'unknown endpoint' })
+// }
+// app.use(unknownEndpoint)
+// app.use(requestLogger)
+
+// app.use(morgan('tiny'))
+
+morgan.token('post', function (req, res) { return JSON.stringify(req.body) });
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post'));
 
 app.use(express.json())
 
 let persons = [
-    
-        {
-            "id": 1,
-            "name": "Arto Hellas",
-            "number": "040-123456"
-        },
-        {
-            "id": 2,
-            "name": "Ada Lovelace",
-            "number": "39-44-5323523"
-        },
-        {
-            "id": 3,
-            "name": "Dan Abramov",
-            "number": "12-43-234345"
-        },
-        {
-            "id": 4,
-            "name": "Mary Poppendieck",
-            "number": "39-23-6423122"
-        }
-    ]
 
-//   const app = http.createServer((request, response) => {
-//     response.writeHead(200, { 'Content-Type': 'application/json' })
-//     response.end(JSON.stringify(notes))
-//   })
+    {
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "040-123456"
+    },
+    {
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
+    },
+    {
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+    },
+    {
+        "id": 4,
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
+    }
+]
+
 
 
 app.get('/', (request, response) => {
@@ -67,11 +82,6 @@ app.delete('/api/persons/:id', (request, response) => {
 })
 
 const generateId = () => {
-    // const maxId = notes.length > 0
-    //     ? Math.max(...persons.map(n => n.id))
-    //     : 0
-    // return maxId + 1
-
     return Math.floor(Math.random() * 10001)
 }
 
@@ -84,7 +94,7 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
-    if (persons.some(p => p.name === body.name)){
+    if (persons.some(p => p.name === body.name)) {
         return response.status(400).json({
             error: 'name must be unique'
         })
