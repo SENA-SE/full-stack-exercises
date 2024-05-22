07b24e106910cbd46c9ac6c38e02cc240875383f
lsot ukow <939991685@qq.com>
2024-05-22 23:22:07 +0300

    9.10-9.11

--- part9/patientor-backend/src/index.ts
+++ part9/patientor-backend/src/index.ts
@@ -1,5 +1,11 @@
 import express from 'express';
+import cors from 'cors';
+import diagnosisRouter from './routes/diagnoses';
+import patientRouter from './routes/patients';
+
+
 const app = express();
+app.use(cors())
 app.use(express.json());
 
 const PORT = 3001;
@@ -8,6 +14,9 @@ app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnosisRouter);
+app.use('/api/patients', patientRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
---
+++ part9/patientor-backend/src/types.ts
@@ -0,0 +1,14 @@
+export interface Diagnose {
+    code: string;
+    name: string;
+    latin?: string;
+}
+
+export interface Patient {
+    id: string;
+    name: string;
+    dateOfBirth: string;
+    gender: string;
+    occupation: string;
+    ssn: string;
+}
